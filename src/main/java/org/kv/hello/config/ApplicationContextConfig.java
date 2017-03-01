b1dde8a0d35d77bd9c7b34a0fbc00e53e8382071
vanvibig <vanvibig@gmail.com>
2017-03-01 19:18:17 +0700

    7h17PM, 1/3/2017

---
+++ src/main/java/org/kv/hello/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package org.kv.hello.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("org.kv.hello.*")
+public class ApplicationContextConfig {
+
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+
+}
---
+++ src/main/java/org/kv/hello/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package org.kv.hello.controller;
+
+/**
+ * Created by KV on 01/03/2017.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("greeting", "Hello Spring MVC");
+
+		return "helloworld";
+
+	}
+
+}
